1317603785f425f03ac1bbfe75100b9b58ee3519
MuteBard <csevere@brandeis.edu>
2020-04-20 01:45:41 -0400

    added first queries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello")
+import { GraphQLServer } from 'graphql-yoga'
+
+//Type-definitions (schema)
+const typeDefs = `
+    type Query {
+        hello : String!
+        name : String!
+    }
+`
+//Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'this is my first Query'
+        },
+        name(){
+            return 'MuteBard'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,resolvers
+})
+
+server.start(() => {
+    console.log("The GraphQL server is up")
+})
