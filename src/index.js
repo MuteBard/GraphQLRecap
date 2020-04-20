3723c70e0a9c6f7fa8759eb73e5ac394c7573ffc
MuteBard <csevere@brandeis.edu>
2020-04-20 02:15:53 -0400

    adding custom typeDefs

--- src/index.js
+++ src/index.js
@@ -5,30 +5,42 @@ import { GraphQLServer } from 'graphql-yoga'
 //contains the five scalar types example:
 const typeDefs = `
     type Query {
-        id: ID!
+        me: User!
+        post: Post!
+    }
+
+    type User { 
+        id : ID!
         name : String!
+        email : String!
         age : Int!
-        employed : Boolean!
-        gpa : Float
+    }
+
+    type Post {
+        id : ID!
+        title : String!
+        body : String!
+        published : String!
     }
 `
 //Resolvers
 const resolvers = {
     Query: {
-        id() {
-            return 'abc123'
+        me() {
+            return {
+                id : "123abc",
+                name : "MuteBard",
+                email : "someemail@gmail.com",
+                age : 27
+            }
         },
-        name(){
-            return 'MuteBard'
-        },
-        age(){
-            return 27
-        },
-        employed(){
-            return true
-        },
-        gpa(){
-            return null
+        post(){
+            return{
+                id : "123xyz",
+                title : "I enjoyed this",
+                body : "Ill about to discuss why this is great, blah blah blah",
+                published : "02:12AM 20 April 2020"
+            }
         }
     }
 }
@@ -40,3 +52,20 @@ const server = new GraphQLServer({
 server.start(() => {
     console.log("The GraphQL server is up")
 })
+
+// query{
+// 	me{
+//     name
+//     age
+//   }
+// }
+
+
+// {
+//     "data": {
+//       "me": {
+//         "name": "MuteBard",
+//         "age": 27
+//       }
+//     }
+//   }
