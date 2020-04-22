26ba8e6dbddce3a474420e2f8a09ef14b2fc4261
MuteBard <csevere@brandeis.edu>
2020-04-22 03:24:51 -0400

    undertanding how to use pass arrays as arguments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ import { GraphQLServer } from 'graphql-yoga'
 //Type-definitions (schema)
 
 //contains the five scalar types example:
+
+const users = [{
+        id : '1',
+        name : 'Carl',
+        email : 'abc@gmail.com'
+    },
+    {
+        id : '2',
+        name : 'Erin',
+        email : 'abc@gmail.com'
+    },
+    {
+        id : '3',
+        name : 'Carla',
+        email : 'abc@gmail.com'
+    }
+]
+
 const typeDefs = `
     type Query {
+        users(name: String!) : [User!]!
         getBugBaseOnMonth(month : String!) : String!
+        getQuarterBlockPrices : [Int!]!
+        getAveragePrice(prices : [Int!]!) : Int!
         me: User!
         post: Post!
     }
@@ -14,7 +35,7 @@ const typeDefs = `
         id : ID!
         name : String!
         email : String!
-        age : Int!
+        age : Int
     }
 
     type Post {
@@ -28,11 +49,25 @@ const typeDefs = `
 const resolvers = {
     Query: {
 
+        users(parent, args, ctx, info){
+            console.log(args.name)
+            return users.filter(user => user.name.toLowerCase().includes(args.name.toLowerCase()))
+        },
 
         getBugBaseOnMonth(parent, args, ctx, info){
             console.log(args)
             return `This is the list of bugs on ${args.month}: Bee`
         },
+
+        getQuarterBlockPrices(parent, args, ctx, info){
+            return [983,923,874,903]
+        },
+
+        getAveragePrice(parent, args, ctx, info){
+            if(args.prices.length == 0) return 0
+            else return Math.floor((args.prices.reduce((sum, elem) => elem + sum))/args.prices.length)
+        },
+
         me() {
             return {
                 id : "123abc",
@@ -74,5 +109,75 @@ server.start(() => {
 //         "name": "MuteBard",
 //         "age": 27
 //       }
+//     }
+//   }
+
+
+
+
+
+
+
+// query{
+//     users{
+//       name
+//     }
+//     getBugBaseOnMonth(month : "MAR")
+//     getQuarterBlockPrices
+//     getAveragePrice(prices:[983,923,874,903])
+//   }
+
+  
+//   {
+//     "data": {
+//       "users": [
+//         {
+//           "name": "Carl"
+//         },
+//         {
+//           "name": "Erin"
+//         },
+//         {
+//           "name": "Carla"
+//         }
+//       ],
+//       "getBugBaseOnMonth": "This is the list of bugs on MAR: Bee",
+//       "getQuarterBlockPrices": [
+//         983,
+//         923,
+//         874,
+//         903
+//       ],
+//       "getAveragePrice": 920
+//     }
+//   }
+
+// query{
+//     users(name: "Erin"){
+//       id
+//       name
+//     }
+//     getBugBaseOnMonth(month : "MAR")
+//     getQuarterBlockPrices
+//     getAveragePrice(prices:[983,923,874,903])
+//   }
+
+
+// {
+//     "data": {
+//       "users": [
+//         {
+//           "id": "2",
+//           "name": "Erin"
+//         }
+//       ],
+//       "getBugBaseOnMonth": "This is the list of bugs on MAR: Bee",
+//       "getQuarterBlockPrices": [
+//         983,
+//         923,
+//         874,
+//         903
+//       ],
+//       "getAveragePrice": 920
 //     }
 //   }
